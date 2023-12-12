df323c278a849afd945705bf433d187e0119811b
ALQETRE <alqetrea@gmail.com>
2023-12-12 21:05:32 +0100

    1.1.1

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pandas as pd
 from js import document
 
+df1 = pd.DataFrame(columns=["name", "email"])
+
 def test(*args):
-  print(document.getElementById("Name").value)
+  print(document.getElementById("name").value)
+
+def update_dataset(*args):
+    name = document.getElementById("name").value
+    email = document.getElementById("email").value
+
+    df1.append([name, email])
