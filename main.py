5492a516b0e35d837fa4f52be108209c4c2a0d02
ALQETRE <alqetrea@gmail.com>
2023-12-13 10:03:38 +0100

    m

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import pandas as pd
 from js import document
 
-df1 = pd.DataFrame(columns=["name", "email"])
+df = pd.DataFrame(columns=["name", "email"])
 
 def update_dataset(*args):
+    df = pd.read_csv(".\\database.csv")
+
     name = document.getElementById("name").value
     email = document.getElementById("email").value
 
     if name == "" or email == "":
         return
 
-    if not df1["email"].isin([email]).any():
-        df1.loc[-1] = [name, email]
-        df1.reset_index(drop= True, inplace= True)
+    if not df["email"].isin([email]).any():
+        df.loc[-1] = [name, email]
+        df.reset_index(drop= True, inplace= True)
 
-    print(df1)
+    pd.to_csv(".\\database.csv")
